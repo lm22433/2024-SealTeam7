6eedc4998d96b072b2d35740cd355fa65f46e025
Finn <sd22404@brostol.ac.uk>
2025-01-16 17:42:24 +0000

    we did it!

--- SealTeam7/Assets/Scripts/TerrainDeformation.cs
+++ SealTeam7/Assets/Scripts/TerrainDeformation.cs
@@ -6,35 +6,36 @@ public class TerrainDeform : MonoBehaviour
     private Texture2D heightMap;
     private TerrainData _terrainData;
     private float[,] _heights;
+
+    private bool _updated; 
     
     void Awake()
     {
+        _heights = new float[heightMap.width, heightMap.height];
         _terrainData = GetComponentInParent<Terrain>().terrainData;
     }
     
     void Update()
     {
-        _heights = new float[heightMap.width, heightMap.height];
-        if (!heightMap) return;
-        
-        for (int y = 0; y < heightMap.height; y++)
+        if (!_updated || !heightMap)
         {
-            for (int x = 0; x < heightMap.width; x++)
+            for (int y = 0; y < heightMap.height; y++)
             {
-                _heights[x, y] = heightMap.GetPixel(x, y).a;
-                //Debug.Log(heightMap.GetPixel(x, y).a);
-                //Debug.Log(_heights[x, y]);
+                for (int x = 0; x < heightMap.width; x++)
+                {
+                    _heights[x, y] = heightMap.GetPixel(x, y).r;
+                }
             }
+            
+            _terrainData.SetHeightsDelayLOD(0, 0, _heights);
+            _terrainData.SyncHeightmap();
+            _updated = true;
         }
-        
-        _terrainData.SetHeightsDelayLOD(0, 0, _heights);
-        _terrainData.SyncHeightmap();
-        enabled = false;
     }
 
     
     private void OnValidate()
     {
-        enabled = true;
+        _updated = false;
     }
 }
