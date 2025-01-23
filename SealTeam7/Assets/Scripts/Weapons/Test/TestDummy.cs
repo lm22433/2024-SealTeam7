eb77da57e3756dfe54f8716f736355bffb49f7f1
PeterLillistone <jr22741@bristol.ac.uk>
2025-01-23 10:29:38 +0000

    Revert "add health bars to test dummies"
    This reverts commit e1bce7a7057a940b47fe91ec5c430a815d6c311a.

--- SealTeam7/Assets/Scripts/Weapons/Test/TestDummy.cs
+++ SealTeam7/Assets/Scripts/Weapons/Test/TestDummy.cs
@@ -1,28 +1,14 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Weapons.Test
 {
     public class TestDummy : MonoBehaviour, IDamageable
     {
-        [SerializeField] private float maxHealth = 150f;
-        [SerializeField] private Slider healthBar;
-        [SerializeField] private Transform player;
-        private float health;
+        [SerializeField] private float health = 150f;
 
-        public void Start()
-        {
-            health = maxHealth;
-            healthBar.maxValue = maxHealth;
-            healthBar.value = health;
-        }
-        
         public void TakeDamage(float dmg)
         {
             health -= dmg;
-            healthBar.value = health;
-            
             Debug.Log($"{gameObject.name} took {dmg} damage. Remaining health: {health}");
 
             if (health <= 0)
@@ -35,11 +21,6 @@ namespace Weapons.Test
         {
             Debug.Log($"{gameObject.name} has died!");
             Destroy(gameObject);
-        }
-
-        private void Update()
-        {
-            healthBar.transform.LookAt(player.position);
         }
     }
 }
