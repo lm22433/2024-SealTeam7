8faa4663737290f5612f10da8e5def5cae61cbf8
Sergi Lange-Soler <sergi.lange123@gmail.com>
2025-01-26 15:03:26 +0000

    Refactoring, debugging

--- SealTeam7/Assets/Scripts/PythonManager.cs
+++ SealTeam7/Assets/Scripts/PythonManager.cs
@@ -1,52 +1,50 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Text;
-using Newtonsoft.Json.Linq;
 using UnityEngine;
 
-namespace DefaultNamespace
+public class PythonManager : MonoBehaviour
 {
-    public class PythonManager : MonoBehaviour
-    {
-        private void Start()
-        {
-            Main();
-        }
+    // // Temporary way to test
+    // private void Start()
+    // {
+    //     Main();
+    // }
         
-
-        static void Main()
+    
+    [ContextMenu("Main")]
+    void Main()
+    {
+        try
         {
-            try
+            using (TcpClient client = new TcpClient("localhost", 9455))
+            using (NetworkStream stream = client.GetStream())
             {
-                using (TcpClient client = new TcpClient("localhost", 9455))
-                using (NetworkStream stream = client.GetStream())
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                Debug.Log("Connected to the Python server.");
+                    
+                stream.Write(Encoding.UTF8.GetBytes("START"));
+                Debug.Log("Sent: START");
+                    
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
                 {
-                    byte[] buffer = new byte[1024];
-                    int bytesRead;
+                    string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Debug.Log($"Received: {receivedData}");
 
-                    Debug.Log("Connected to the Python server.");
-                    
-                    stream.Write(Encoding.UTF8.GetBytes("START"));
-                    Debug.Log("Sent: START");
-                    
-                    while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
-                    {
-                        string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                        Debug.Log($"Received: {receivedData}");
-
-                        // Parse JSON data
-                        // var data = JObject.Parse(receivedData);
-                        // string objName = data["objects"].ToString();
-                        // int x = data["x"].ToObject<int>();
-                        // int y = data["y"].ToObject<int>();
-                        // Console.WriteLine($"Object: {objName}, X: {x}, Y: {y}");
-                    }
+                    // Parse JSON data
+                    // var data = JObject.Parse(receivedData);
+                    // string objName = data["objects"].ToString();
+                    // int x = data["x"].ToObject<int>();
+                    // int y = data["y"].ToObject<int>();
+                    // Console.WriteLine($"Object: {objName}, X: {x}, Y: {y}");
                 }
             }
-            catch (Exception ex)
-            {
-                Debug.Log($"Error: {ex.Message}");
-            }
+        }
+        catch (Exception ex)
+        {
+            Debug.Log($"Error: {ex.Message}");
         }
     }
 }
--- python/main.py
+++ python/main.py
@@ -1,102 +1,123 @@
+import os
 import socket
 import json
+import threading
+
 import time
 import cv2
 import mediapipe as mp
+import psutil
 from mediapipe.tasks.python import BaseOptions
 from mediapipe.tasks.python.components.containers.detections import DetectionResult
 from mediapipe.tasks.python.vision import ObjectDetector, ObjectDetectorOptions, RunningMode
 
 
 model_path = 'model.tflite'
-object_detection_done = False
+object_detection_result = None
+object_detection_done = threading.Event()
 
 
 def object_detector_callback(result, output_image, timestamp_ms):
+    global object_detection_result
+
     print(f'detection result: {result}')
-    
-    
-def inference_frame(frame):
+    object_detection_result = result
+    object_detection_done.set()
+
+
+def inference_frame(object_detector, frame):
     mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
     timestamp_ms = int(time.time() * 1000)
     object_detector.detect_async(mp_image, timestamp_ms)
-    # block until callback called
-    return {"objects": [{"type": "spawner", "x": 34, "y": 52}]}
+    object_detection_done.wait()  # block until object_detector_callback called
+    object_detection_done.clear()
+    return {"objects": [{"type": detection.categories[0].category_name,
+                         "x": detection.bounding_box.origin_x + detection.bounding_box.width / 2,
+                         "y": detection.bounding_box.origin_y + detection.bounding_box.height / 2}
+                        for detection in object_detection_result.detections]}
 
 
-options = ObjectDetectorOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=RunningMode.LIVE_STREAM,
-    max_results=5,
-    result_callback=object_detector_callback)
-
-object_detector = ObjectDetector.create_from_options(options)
-
-
-def handle_client(client_socket):
-    client_socket.setblocking(False)  # when receiving, if nothing is in the buffer just skip
+def handle_client(conn):
+    conn.setblocking(True)  # block until START control signal received
     object_detection_running = False
     video_capture = None
 
-    while True:
-        try:
-            # Check if any control signals in buffer
+    options = ObjectDetectorOptions(
+        base_options=BaseOptions(model_asset_path=model_path),
+        running_mode=RunningMode.LIVE_STREAM,
+        max_results=5,
+        result_callback=object_detector_callback)
+
+    with ObjectDetector.create_from_options(options) as object_detector:
+        while True:
             try:
-                message = client_socket.recv(1024).decode()
-                if message:
-                    message = message.strip()
-                    print(f"Control signal received: {message}")
-                    if message == "START":
-                        object_detection_running = True
-                        video_capture = cv2.VideoCapture(0)
-                    elif message == "STOP":
-                        object_detection_running = False
-                        video_capture.release()
-                    elif message == "EXIT":
-                        break
-            except BlockingIOError:
-                pass  # No control signal in buffer, continue
+                # Check if any control signals in buffer
+                if not object_detection_running:
+                    print("Waiting for START control signal...")
+                try:
+                    message = conn.recv(1024).decode()
+                    if message:
+                        message = message.strip()
+                        print(f"Control signal received: {message}")
+                        if message == "START":
+                            object_detection_running = True
+                            conn.setblocking(False)  # no longer should block and wait for control signals
+                            video_capture = cv2.VideoCapture(0)
+                        elif message == "STOP":
+                            object_detection_running = False
+                            conn.setblocking(True)  # block until START control signal received again
+                            video_capture.release()
+                        elif message == "EXIT":
+                            break
+                except BlockingIOError:
+                    pass  # No control signal in buffer - that's normal, continue
 
-            if object_detection_running:
-                success, frame = video_capture.read()
-                if success:
-                    data = inference_frame(frame)
-                    data_json = json.dumps(data)
-                    client_socket.send(data_json.encode())
-                else:
-                    print("Failed to read frame.")
+                # Main logic - read frame from camera, run object detection, send result to client
+                if object_detection_running:
+                    success, frame = video_capture.read()
+                    if success:
+                        data = inference_frame(object_detector, frame)
+                        data_json = json.dumps(data)
+                        conn.send(data_json.encode())
+                    else:
+                        print("Failed to read frame.")
 
-        except ConnectionResetError:
-            print("Connection lost.")
-            break
-            
-        except KeyboardInterrupt:
-            print("Shutting down server.")
-            break
-            
-        except Exception as error:
-            print(error)
-            print("Error, shutting down server.")
-            break
+            except ConnectionResetError:
+                print("Connection lost.")
+                break
 
-        time.sleep(0.1)
+            except KeyboardInterrupt:
+                print("Shutting down server.")
+                break
+
+            except Exception as error:
+                print(error)
+                print("Error, shutting down server.")
+                break
 
     # clean up
     if video_capture is not None and video_capture.isOpened():
         video_capture.release()
-    client_socket.close()
-    object_detector.close()
 
 
 def start_server(host="127.0.0.1", port=9455):  # 127.0.0.1 is localhost
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((host, port))
-    server.listen(5)
-    print(f"Server listening on {host}:{port}")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.bind((host, port))
+        server.listen(5)
+        print(f"Server listening on {host}:{port}")
 
-    client_socket, addr = server.accept()
-    print(f"Connection from {addr}")
-    handle_client(client_socket) 
+        conn, addr = server.accept()
+        print(f"Connection from {addr}")
+        with conn:
+            handle_client(conn)
 
 
 start_server()
+
+# # debug why it stops with SIGSEGV
+# for thread in threading.enumerate():
+#     print(f"Thread name: {thread.name}, Is daemon: {thread.daemon}, Is alive: {thread.is_alive()}")
+# proc = psutil.Process()
+# print("Open files:", proc.open_files())
+# print("Child processes:", proc.children())
+# print("Threads:", proc.threads())
