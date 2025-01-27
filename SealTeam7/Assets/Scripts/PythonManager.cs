00cde8df9a0077bb49269a0daa9e14cb6d2cba12
Sergi Lange-Soler <sergi.lange123@gmail.com>
2025-01-27 18:12:22 +0000

    Properly implementing PythonManager (WIP)

--- SealTeam7/Assets/Scripts/PythonManager.cs
+++ SealTeam7/Assets/Scripts/PythonManager.cs
@@ -3,30 +3,46 @@ using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
 
-public class PythonManager : MonoBehaviour
+public static class PythonManager
 {
-    // // Temporary way to test
-    // private void Start()
-    // {
-    //     Main();
-    // }
-        
+    private const string Host = "localhost";
+    private const int Port = 9455;
+    private static Encoding _encoding = Encoding.UTF8;
+    
+    private static TcpClient _client;
+    private static NetworkStream _stream;
+    
+    private static byte[] _buffer = new byte[1024];
+    private static int _bytesRead;
+    
+    public static void Connect()
+    {
+        if (_client != null)
+        {
+            Debug.Log("Already connected to the Python server.");
+            return;
+        }
+        _client = new TcpClient("localhost", 9455);
+        _stream = _client.GetStream();
+        Debug.Log("Connected to the Python server.");
+    }
+    
+    
+    public static void StartObjectDetection()
+    {
+        _stream.Write(Encoding.UTF8.GetBytes("START"));
+        Debug.Log("Sent: START");
+    }
     
     [ContextMenu("Main")]
-    void Main()
+    static void Main()
     {
         try
         {
             using (TcpClient client = new TcpClient("localhost", 9455))
             using (NetworkStream stream = client.GetStream())
             {
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-
-                Debug.Log("Connected to the Python server.");
-                    
-                stream.Write(Encoding.UTF8.GetBytes("START"));
-                Debug.Log("Sent: START");
+                
                     
                 while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
                 {
