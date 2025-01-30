58c302557246c72ea990e12344312e89beaedd07
sd22404 <sd22404@bristol.ac.uk>
2025-01-30 09:01:08 +0000

    improvements

--- SealTeam7/Assets/Scripts/Enemies/Enemy.cs
+++ SealTeam7/Assets/Scripts/Enemies/Enemy.cs
@@ -1,22 +1,19 @@
-using System;
+﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
 using Weapons;
 
 namespace Enemies
 {
-    public class Enemy : MonoBehaviour, IDamageable
+    public abstract class Enemy : MonoBehaviour, IDamageable
     {
-        [SerializeField] private float maxHealth = 150f;
-        [SerializeField] private Slider healthBar;
-        [SerializeField] private Transform player;
-        [SerializeField] private float shootRange;
-        [SerializeField] private float shootDelay;
-        
+        [SerializeField] public Slider healthBar;
+        [SerializeField] public Transform player;
+        [SerializeField] protected float maxHealth;
+        [SerializeField] protected float damage;
         private float _health;
-        private float _timeSinceLastShoot;
 
-        public void Start()
+        public virtual void Start()
         {
             _health = maxHealth;
             healthBar.maxValue = maxHealth;
@@ -34,28 +31,15 @@ namespace Enemies
             }
         }
         
-        private void Die()
+        public virtual void Die()
         {
             Destroy(gameObject);
         }
         
-        private void Shoot()
-        {
-            Debug.Log("Shot!");
-            _timeSinceLastShoot = 0;
-        }
+        public abstract void Attack();
 
-        private void Update()
+        public virtual void Update()
         {
-            _timeSinceLastShoot += Time.deltaTime;
-            if ((player.transform.position - transform.position).sqrMagnitude < shootRange * shootRange)
-            {
-                if (_timeSinceLastShoot > shootDelay)
-                {
-                    Shoot();
-                }
-            }
-            
             healthBar.transform.LookAt(player.position);
         }
     }
---
+++ SealTeam7/Assets/Scripts/Enemies/PlayerStats.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Weapons;
+
+namespace Enemies
+{
+    public class PlayerStats : MonoBehaviour, IDamageable
+    {
+        [SerializeField] private float maxHealth;
+        private float _health;
+
+        private void Start()
+        {
+            _health = maxHealth;
+        }
+        
+        public void TakeDamage(float dmg)
+        {
+            _health -= dmg;
+        }
+
+        private void FixedUpdate()
+        {
+            Debug.Log(_health);
+        }
+    }
+}
