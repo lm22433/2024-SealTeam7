558060ca4924c45c7dd82f6c59230d3445476574
Sergi Lange-Soler <sergi.lange123@gmail.com>
2025-02-02 20:51:00 +0000

    Changes to PythonManager test

--- SealTeam7/Assets/Scripts/TestingUtilities.cs
+++ SealTeam7/Assets/Scripts/TestingUtilities.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using System.Threading;
 using Microsoft.Azure.Kinect.Sensor;
 
 public class TestingUtilities : MonoBehaviour
 {
+    private int _pythonTestIndex = 0;
+    
     // ReSharper disable Unity.PerformanceAnalysis
     void Update()
     {
@@ -12,18 +15,31 @@ public class TestingUtilities : MonoBehaviour
             PythonManager.Connect();
             Thread.Sleep(1000);
             PythonManager.StartObjectDetection();
-            Thread.Sleep(1000);
-            Debug.Log(PythonManager.GetSandboxObjects());
-            Thread.Sleep(1000);
-            // PythonManager.SendColorImage(new Image(ImageFormat.ColorBGRA32, 1280, 720));
-            Thread.Sleep(1000);
-            Debug.Log(PythonManager.GetSandboxObjects());
-            Thread.Sleep(1000);
-            Debug.Log(PythonManager.GetSandboxObjects());
-            Thread.Sleep(1000);
-            PythonManager.StopObjectDetection();
-            Thread.Sleep(5000);
-            PythonManager.Disconnect();
+            Thread.Sleep(100);
+            _pythonTestIndex = 1;
         }
+
+        // test PythonManager
+        if (_pythonTestIndex is >= 1 and <= 10)
+        {
+            Debug.Log(ToString(PythonManager.GetSandboxObjects()));
+            Thread.Sleep(500);
+            // PythonManager.SendColorImage(new Image(ImageFormat.ColorBGRA32, 1280, 720));
+            Thread.Sleep(500);
+            _pythonTestIndex++;
+        }
+        else if (_pythonTestIndex > 10)
+        {
+            PythonManager.StopObjectDetection();
+            Thread.Sleep(1000);
+            PythonManager.Disconnect();
+            _pythonTestIndex = 0;
+        }
+    }
+
+
+    public static string ToString<T>(IEnumerable<T> array)
+    {
+        return $"[{string.Join(", ", array)}]";
     }
 }
