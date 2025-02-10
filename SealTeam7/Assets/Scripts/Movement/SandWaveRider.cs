ff5ae468507787ad03ca95b25ad92e3bde6cb4ab
PeterLillistone <jr22741@bristol.ac.uk>
2025-02-10 20:18:43 +0000

    Good enough

--- SealTeam7/Assets/Scripts/Movement/SandWaveRider.cs
+++ SealTeam7/Assets/Scripts/Movement/SandWaveRider.cs
@@ -4,8 +4,10 @@ public class SandWaveRider : MonoBehaviour
 {
     [SerializeField] LayerMask layerMask;
     [SerializeField] float riderSpeed;
-    [SerializeField] float gravityPoint = 1f;
-    [SerializeField] float surfacePoint = 1f;
+    [SerializeField] float startPoint = 1f;
+    [SerializeField] float rangeGap = 1f;
+    [SerializeField] float deadGap = 1f;
+    
     Transform body;
 
     private void Awake() {
@@ -15,19 +17,17 @@ public class SandWaveRider : MonoBehaviour
     {
         RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, gravityPoint, layerMask))
+        if (Physics.Raycast(transform.position + new Vector3(0, startPoint, 0), Vector3.down, out hit, startPoint, layerMask))
         { 
-            Debug.DrawRay(transform.position, Vector3.down * hit.distance, Color.yellow); 
+            Debug.DrawRay(transform.position + new Vector3(0, startPoint, 0), Vector3.down * hit.distance, Color.yellow); 
 
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, surfacePoint, layerMask))
-            { 
-                Debug.DrawRay(transform.position, Vector3.down * hit.distance, Color.blue);
-                body.Translate(new Vector3(0, Mathf.Lerp(0, hit.distance, hit.distance * riderSpeed), 0));
-            }
-            body.GetComponent<Rigidbody>().useGravity = false;
+            float diff = Mathf.Abs(hit.transform.position.y - transform.position.y) / (hit.transform.position.y + transform.position.y);
+
+            body.Translate(new Vector3(0, Mathf.Lerp(0, startPoint - hit.distance, (startPoint - hit.distance / startPoint) * riderSpeed), 0));
+
+
         } else {
-             Debug.DrawRay(transform.position, Vector3.down * gravityPoint, Color.green); 
-             body.GetComponent<Rigidbody>().useGravity = true;
+             Debug.DrawRay(transform.position, Vector3.down * (startPoint + rangeGap / 2), Color.green); 
         }
 
     }
