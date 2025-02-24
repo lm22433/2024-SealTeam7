dfc0d4158fe5ec855e78d3dc2f5e4a258a7c3923
Josh <dw22682@bristol.ac.uk>
2025-02-24 16:10:14 +0000

    lol this was too much fun to make Still lots of changes to be made

---
+++ SealTeam7/Assets/Scripts/Enemies/FunkyPhysics/BasePhysics.cs
@@ -0,0 +1,50 @@
+﻿using Enemies.Utils;
+using Map;
+using Player;
+using UnityEngine;
+
+namespace Enemies.FunkyPhysics
+{
+    public abstract class BasePhysics : MonoBehaviour
+    {
+        [SerializeField] protected float gravityDefiance;
+        [SerializeField] protected float defianceThreshold;
+        [SerializeField] protected float sinkFactor;
+        [SerializeField] protected float fallDeathRequirement;
+        [SerializeField] protected Enemy _self;
+        protected float reasonableGroundedness = 0.6f;
+        protected MapManager _mapManager;
+        protected EnemyManager _enemyManager;
+        protected Rigidbody _rb;
+        
+        protected virtual void Start()
+        {
+            _enemyManager = FindFirstObjectByType<EnemyManager>();
+            _mapManager = FindFirstObjectByType<MapManager>();
+            _rb = GetComponent<Rigidbody>();
+            _rb.freezeRotation = true;
+        }
+
+        protected abstract void DeathAffect(EnemyManager manager);
+
+        protected virtual void Update()
+        {
+            if (transform.position.y < _mapManager.GetHeight(transform.position.x, transform.position.z) - sinkFactor)
+            {
+                Debug.Log("WOULD DIE BURIED");
+                _enemyManager.Kill(_self);
+            }
+
+            else if (_rb.linearVelocity.y > defianceThreshold && transform.position.y < _mapManager.GetHeight(transform.position.x, transform.position.z) + reasonableGroundedness)
+            {
+                _rb.AddForce(Vector3.up * gravityDefiance, ForceMode.Impulse);
+            }
+            else if (-_rb.linearVelocity.y >= fallDeathRequirement && transform.position.y < _mapManager.GetHeight(transform.position.x, transform.position.z) + reasonableGroundedness)
+            {
+                Debug.Log("WOULD DIE FALL DMG");
+                _enemyManager.Kill(_self);
+            }
+        }
+        
+    }
+}
