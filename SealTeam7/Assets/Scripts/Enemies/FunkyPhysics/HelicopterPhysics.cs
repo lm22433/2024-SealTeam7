5881dd37a8141abf8c719fa1998ae0b6f48b2edc
PeterLillistone <jr22741@bristol.ac.uk>
2025-03-03 21:08:55 +0000

    Helichopters done

---
+++ SealTeam7/Assets/Scripts/Enemies/FunkyPhysics/HelicopterPhysics.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Map;
+
+namespace Enemies.FunkyPhysics
+{
+    public class HelicopterPhysics : BasePhysics
+    {
+        protected override void Start()
+        {
+            EnemyManager = FindFirstObjectByType<EnemyManager>();
+            MapManager = FindFirstObjectByType<MapManager>();
+            return;
+        }
+
+        protected override void Update()
+        {
+            if (transform.position.y < MapManager.GetHeight(transform.position.x, transform.position.z))
+            {
+                EnemyManager.Kill(self);
+            }
+
+            return;
+        }
+        
+        private void OnTriggerEnter(Collider collider) {
+            if (collider.gameObject.tag == "Ground") {
+                Debug.Log("collision");
+                GetComponent<Helicopter>().Die();
+            }
+            
+        }
+    }
+}
