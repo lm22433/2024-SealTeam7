1ed434ae599d0010a6ecee235d61c93e1fcfeff2
PeterLillistone <jr22741@bristol.ac.uk>
2025-03-04 10:36:26 +0000

    Helicopter Blade spin

--- SealTeam7/Assets/Scripts/Enemies/FunkyPhysics/HelicopterPhysics.cs
+++ SealTeam7/Assets/Scripts/Enemies/FunkyPhysics/HelicopterPhysics.cs
@@ -5,6 +5,10 @@ namespace Enemies.FunkyPhysics
 {
     public class HelicopterPhysics : BasePhysics
     {
+        [SerializeField] GameObject mainPropeller;
+        [SerializeField] GameObject subPropeller;
+        [SerializeField] float propellerSpeed;
+
         protected override void Start()
         {
             EnemyManager = FindFirstObjectByType<EnemyManager>();
@@ -13,7 +17,10 @@ namespace Enemies.FunkyPhysics
         }
 
         protected override void Update()
-        {
+        {   
+            mainPropeller.transform.Rotate(new Vector3(0, propellerSpeed, 0) * Time.deltaTime);
+            subPropeller.transform.Rotate(new Vector3(0, 0, propellerSpeed) * Time.deltaTime);
+
             if (transform.position.y < MapManager.GetHeight(transform.position.x, transform.position.z))
             {
                 EnemyManager.Kill(self);
