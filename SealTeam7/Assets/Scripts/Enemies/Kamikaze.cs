3c79e7981c4267fcfc423babee46d6c6ad903da5
Josh <dw22682@bristol.ac.uk>
2025-03-05 15:15:18 +0000

    started adding kamikaze planes

---
+++ SealTeam7/Assets/Scripts/Enemies/Kamikaze.cs
@@ -0,0 +1,82 @@
+﻿using Player;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class Kamikaze : Enemy
+    {
+        [SerializeField] float flyHeight;
+        [SerializeField] private ParticleSystem deathParticles;
+        [SerializeField] private ParticleSystem trail;
+        [SerializeField] private ParticleSystem smokeTrail;
+        [SerializeField] private ParticleSystem chargeParticles;
+
+        private float _charge;
+
+        private void Awake() {
+            transform.position = new Vector3(transform.position.x, flyHeight, transform.position.z);
+        }
+        
+        protected override void Attack(PlayerDamageable target)
+        {
+            target?.TakeDamage(attackDamage);
+        }
+        
+        protected override void EnemyUpdate()
+        {
+
+            TargetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, Quaternion.LookRotation(Target.transform.position - transform.position).eulerAngles.y, transform.rotation.eulerAngles.z);
+            TargetDirection = (Target.transform.position - transform.position + Vector3.up * (transform.position.y - Target.transform.position.y)).normalized;
+            
+            if (State == EnemyState.AttackCore || State == EnemyState.AttackHands) _charge += Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, flyHeight, transform.position.z);
+        }
+
+        protected override void EnemyFixedUpdate()
+        {
+            switch (State)
+            {
+                case EnemyState.Moving:
+                {
+                    if(!trail.isPlaying) trail.Play();
+                    if(!smokeTrail.isPlaying) smokeTrail.Play();
+                    chargeParticles.Stop();
+                    Rb.MoveRotation(TargetRotation);
+                    Rb.AddForce(TargetDirection * (moveSpeed * 10f));
+
+                    break;
+                }
+                case EnemyState.AttackCore:
+                {
+                    //trail.Stop();
+                    if(!chargeParticles.isPlaying) chargeParticles.Play();
+                    smokeTrail.Stop();
+                    if (_charge > attackInterval)
+                    {
+                        Attack(EnemyManager.godlyCore);
+                        State = EnemyState.Dying;
+                    }
+                    break;
+                }
+                case EnemyState.AttackHands:
+                {
+                    //trail.Stop();
+                    if(!chargeParticles.isPlaying) chargeParticles.Play();
+                    smokeTrail.Stop();
+                    if (_charge > attackInterval)
+                    {
+                        Attack(EnemyManager.godlyHands);
+                        State = EnemyState.Dying;
+                    }
+                    break;
+                }
+            }
+        }
+
+        public override void SetupDeath()
+		{
+			deathParticles.Play();
+			State = EnemyState.Dying;
+		}
+    }
+}
