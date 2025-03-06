ea16bf2c7754efd91191d22d155396e6a49abaf6
Harry <lm22433@bristol.ac.uk>
2025-03-06 20:42:43 +0000

    helicopter helicopter

--- SealTeam7/Assets/Scripts/Enemies/FunkyPhysics/HelicopterPhysics.cs
+++ SealTeam7/Assets/Scripts/Enemies/FunkyPhysics/HelicopterPhysics.cs
@@ -5,33 +5,29 @@ namespace Enemies.FunkyPhysics
 {
     public class HelicopterPhysics : BasePhysics
     {
-        [SerializeField] GameObject mainPropeller;
-        [SerializeField] GameObject subPropeller;
-        [SerializeField] float propellerSpeed;
+        [SerializeField] private Transform mainPropeller;
+        [SerializeField] private Transform subPropeller;
+        [SerializeField] private float propellerSpeed;
 
         protected override void Start()
         {
             EnemyManager = FindFirstObjectByType<EnemyManager>();
             MapManager = FindFirstObjectByType<MapManager>();
-            return;
         }
 
         protected override void Update()
         {   
-            mainPropeller.transform.Rotate(new Vector3(0, propellerSpeed, 0) * Time.deltaTime);
-            subPropeller.transform.Rotate(new Vector3(0, 0, propellerSpeed) * Time.deltaTime);
+            mainPropeller.Rotate(Vector3.forward * (propellerSpeed * Time.deltaTime)); // Kind of fucked. Jank Blender. Don't touch.
+            subPropeller.Rotate(Vector3.forward * (propellerSpeed * Time.deltaTime));
 
             if (transform.position.y < MapManager.GetHeight(transform.position.x, transform.position.z))
             {
                 EnemyManager.Kill(self);
             }
-
-            return;
         }
         
         private void OnTriggerEnter(Collider collider) {
             if (collider.gameObject.tag == "Ground") {
-                Debug.Log("collision");
                 GetComponent<Helicopter>().Die();
             }
             
