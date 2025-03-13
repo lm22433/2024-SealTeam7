211acd7b9e6c30cd41c773277109c474b343458d
Rolodophone <rolodophone@gmail.com>
2025-03-13 15:18:14 +0000

    Move Gestures to separate file

--- SealTeam7/Assets/Scripts/Python/Gestures.cs
+++ SealTeam7/Assets/Scripts/Python/Gestures.cs
@@ -4,5 +4,17 @@ namespace Python
     {
         public int Left;
         public int Right;
+
+        // Gestures:
+        // None (0)
+        // Closed_Fist (1)
+        // Open_Palm (2)
+        // Pointing_Up (3)
+        // Thumb_Down (4)
+        // Thumb_Up (5)
+        // Victory (6)
+        // Spock (7)
+        // Calling (8)
+        // ILoveYou (9)
     }
 }
