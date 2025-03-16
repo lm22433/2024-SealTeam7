15c2670f5f67e3f677682f5a0628d0aa218ba7be
Josh <dw22682@bristol.ac.uk>
2025-03-16 17:33:01 +0000

    added air spawner looks shit plays well as far as i can tell. Para troops seem to work as well

---
+++ SealTeam7/Assets/Scripts/Enemies/ArialSpawner.cs
@@ -0,0 +1,42 @@
+﻿using Enemies.Utils;
+using Player;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class ArialSpawner : Enemy
+    {
+        [SerializeField] float flyHeight;
+        [SerializeField] private Transform muzzle;
+        [SerializeField] private GameObject projectile;
+        [SerializeField] private GameObject spawnee;
+
+        protected override void Start()
+        {
+            base.Start();
+            LastAttack = attackInterval - 2.0f;
+        }
+        
+        private void Awake()
+        {
+            transform.position = new Vector3(transform.position.x, flyHeight, transform.position.z);
+        }
+        
+        protected override void Attack(PlayerDamageable toDamage)
+        {
+            EnemyManager.SpawnerSpawn(new Vector3(transform.position.x, transform.position.y + 2.0f, transform.position.z - 2.0f), spawnee, attackDamage);
+        }
+        
+        protected override void EnemyUpdate()
+        {
+            TargetRotation = Quaternion.Euler(transform.eulerAngles.x, Quaternion.LookRotation(TargetPosition - transform.position).eulerAngles.y, transform.eulerAngles.z);
+            TargetDirection = new Vector3(transform.forward.x, 0f, transform.forward.z).normalized;
+        }
+
+        protected override void EnemyFixedUpdate()
+        {
+            if (Rb.position.y > flyHeight) Rb.AddForce(Vector3.down, ForceMode.Impulse);
+            if (Rb.position.y < flyHeight) Rb.AddForce(Vector3.up, ForceMode.Impulse);
+        }
+    }
+}
