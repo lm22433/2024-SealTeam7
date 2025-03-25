c3817141ffc6fb21caafa5fb9e6a88db3f191a53
PeterLillistone <jr22741@bristol.ac.uk>
2025-03-25 16:34:33 +0000

    Change Cams

--- SealTeam7/Assets/Scripts/UI/BattleCamController.cs
+++ SealTeam7/Assets/Scripts/UI/BattleCamController.cs
@@ -2,69 +2,76 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-using battleCam {
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BattleCamController : MonoBehaviour
+{
     [Serializable]
     public struct BattleCamera
     {
         public GameObject cam;
         public bool isActive;
-        public RenderTexture[] battleCameras;
     }
 
-    public class BattleCamController : MonoBehaviour
+    [Header("Game Objects")]
+    [SerializeField] private RenderTexture[] renderTextures;
+    [SerializeField] private List<BattleCamera> battleCameras = new List<BattleCamera>();
+    [SerializeField] private int currentMainCamera = 0;
+
+    [SerializeField, Range(10f, 100f)] private float minCameraWaitTime;
+    [SerializeField, Range(10f, 100f)] private float maxCameraWaitTime;
+
+    [Header("Scrolling Headlines")]
+    [SerializeField] private TMP_Text scrollingHeadlines;
+    [SerializeField] private float maxScroll;
+    [SerializeField] private float currentScroll = 0;
+    [SerializeField] private float scrollSpeed;
+
+    void Start()
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        //Display.displays[1].Activate();
 
-        [Header("Game Objects")]
-        [SerializeField] private BattleCamera[] battleCameras;
-        [SerializeField] private RawImage[] cameraTiles;
-        [SerializeField] private int currentMainCamera = 0;
+        StartCoroutine(waitToChangeCameraPositions());
+    }
 
-        [SerializeField, Range(10f, 100f)] private float minCameraWaitTime;
-        [SerializeField, Range(10f, 100f)] private float maxCameraWaitTime;
+    private void FixedUpdate() {
+        scrollHeading();
 
-        [Header("Scrolling Headlines")]
-        [SerializeField] private TMP_Text scrollingHeadlines;
-        [SerializeField] private float maxScroll;
-        [SerializeField] private float currentScroll = 0;
-        [SerializeField] private float scrollSpeed;
 
-        void Start()
-        {
-            Display.displays[1].Activate();
+    }
 
-            waitToChangeCameraPositions()
+    private IEnumerator waitToChangeCameraPositions()
+    {
+        int wait_time = (int)UnityEngine.Random.Range(minCameraWaitTime, maxCameraWaitTime);
+        yield return new WaitForSeconds (wait_time);
+
+        swapCameraPositions();
+        StartCoroutine(waitToChangeCameraPositions());
+    }
+
+    private void swapCameraPositions() {
+        List<BattleCamera> subCamera = new List<BattleCamera>(battleCameras);
+        
+        for(int i = 0; i < renderTextures.Length; i++) {
+            int index = (int)UnityEngine.Random.Range(0, renderTextures.Length);
+            Debug.Log(index);
+
+            subCamera[index].cam.GetComponent<Camera>().targetTexture = renderTextures[i];
+            subCamera.RemoveAt(index);
         }
 
-        private void FixedUpdate() {
-            scrollHeading();
 
+    }
 
-        }
+    private void scrollHeading() {
+        currentScroll += scrollSpeed;
+        scrollingHeadlines.gameObject.transform.Translate(new Vector3(-scrollSpeed, 0, 0));
 
-        private IEnumerator waitToChangeCameraPositions()
-        {
-            int wait_time = Random.Range(minCameraWaitTime, maxCameraWaitTime);
-            yield return new WaitForSeconds (wait_time);
-            swapCameraPositions();
-        }
-
-        private swapCameraPositions() {
-            Array.Sort(battleCameras, (x, y) => x.isActive.CompareTo(y.isActive));
-            
-            int wait_time = Random.Range(0, );
-
-
-        }
-
-        private void scrollHeading() {
-            currentScroll += scrollSpeed;
-            scrollingHeadlines.gameObject.transform.Translate(new Vector3(-scrollSpeed, 0, 0));
-
-            if (currentScroll >= maxScroll) {
-                scrollingHeadlines.gameObject.transform.Translate(new Vector3(maxScroll, 0, 0));
-                currentScroll = 0;
-            }
+        if (currentScroll >= maxScroll) {
+            scrollingHeadlines.gameObject.transform.Translate(new Vector3(maxScroll, 0, 0));
+            currentScroll = 0;
         }
     }
 }
