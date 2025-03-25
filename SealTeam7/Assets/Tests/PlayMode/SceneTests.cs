ef99602702c1d61edfc8605055436576fce61bc5
Harry Greentree <lm22433@bristol.ac.uk>
2025-03-25 11:37:32 +0000

    Fix playmode scene tests

--- SealTeam7/Assets/Tests/PlayMode/SceneTests.cs
+++ SealTeam7/Assets/Tests/PlayMode/SceneTests.cs
@@ -1,37 +1,38 @@
+using System.Collections;
 using NUnit.Framework;
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
 
 namespace Tests.PlayMode
 {
     public class SceneTests
     {
-        [SetUp]
-        public void Setup()
-        {
-            SceneManager.LoadScene("Assets/Scenes/TerrainTesting.unity");
-        }
-    
-        [Test]
-        public void VerifyGameManagerExistsInScene()
+        [OneTimeSetUp]
+        public void OneTimeSetup() => SceneManager.LoadScene("Assets/Scenes/TerrainTesting.unity", LoadSceneMode.Single);
+        
+        [UnityTest]
+        public IEnumerator VerifyGameManagerExistsInScene()
         {
             GameObject gameObject = GameObject.Find("GameManager");
             Assert.That(gameObject, Is.Not.Null);
+            yield return null;
         }
     
-        [Test]
-        public void VerifyMapManagerExistsInScene()
+        [UnityTest]
+        public IEnumerator VerifyMapManagerExistsInScene()
         {
             GameObject gameObject = GameObject.Find("MapManager");
             Assert.That(gameObject, Is.Not.Null);
+            yield return null;
         }
     
-        [Test]
-        public void VerifyEnemyManagerExistsInScene()
+        [UnityTest]
+        public IEnumerator VerifyEnemyManagerExistsInScene()
         {
             GameObject gameObject = GameObject.Find("EnemyManager");
             Assert.That(gameObject, Is.Not.Null);
+            yield return null;
         }
     }
 }
