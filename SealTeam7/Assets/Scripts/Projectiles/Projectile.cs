57c59450f3d7837f97cf214c5f90d358899ff28c
sd22404 <sd22404@bristol.ac.uk>
2025-04-02 21:30:10 +0100

    broken mortar shooting

--- SealTeam7/Assets/Scripts/Projectiles/Projectile.cs
+++ SealTeam7/Assets/Scripts/Projectiles/Projectile.cs
@@ -17,9 +17,22 @@ namespace Projectiles
         public ProjectileType projectileType;
         
         private Vector3 _startPosition;
+        private Vector3 _gravity;
 
         public void Init()
         {
+            _startPosition = transform.position;
+            _gravity = new Vector3(0f, -9.81f, 0f);
+            
+            if (projectileType == ProjectileType.MortarProjectile)
+            {
+                var rb = gameObject.AddComponent<Rigidbody>();
+                var t = 10f;
+                var p = TargetPosition - _startPosition;
+                //rb.useGravity = false;
+                rb.linearVelocity = new Vector3(p.x / t, p.y / t + 0.5f * _gravity.y * t, p.z / t);
+            }
+            
             if (trail) trail.Clear();
         }
 
@@ -31,8 +44,10 @@ namespace Projectiles
                 ProjectilePool.GetInstance().ReturnToPool(projectileType, gameObject);
                 return;
             }
+            
+            //if (TryGetComponent(out Rigidbody rb)) rb.AddForce(_gravity, ForceMode.Acceleration);
 
-            transform.position = Vector3.MoveTowards(transform.position, TargetPosition, speed * Time.deltaTime);
+            if (projectileType != ProjectileType.MortarProjectile) transform.position = Vector3.MoveTowards(transform.position, TargetPosition, speed * Time.deltaTime);
 
             if (transform.position == TargetPosition)
             {
