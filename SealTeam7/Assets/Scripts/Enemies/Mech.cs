ab4595cd41c04e7796e914513689d0c899282d5f
sd22404 <sd22404@bristol.ac.uk>
2025-04-02 14:33:41 +0100

    UNTESTED mech projectile

--- SealTeam7/Assets/Scripts/Enemies/Mech.cs
+++ SealTeam7/Assets/Scripts/Enemies/Mech.cs
@@ -1,31 +1,59 @@
 using Enemies.Utils;
 using Map;
+using Player;
+using Projectiles;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Enemies
 {
     public class Mech : Enemy
     {
-        [SerializeField] private GameObject Mmodel;
-        [SerializeField] private GameObject Msphere;
-        [SerializeField] private Rigidbody primaryRB;
+        [SerializeField] private GameObject mmodel;
+        [SerializeField] private GameObject msphere;
+        [SerializeField] private Rigidbody primaryRb;
         [SerializeField] private LayerMask whatIsGround;
         
         [Header("Movement")]
         [SerializeField] private float rollSpeed;
         [SerializeField] private float tolerance;
-        [SerializeField] private Transform NW;
-        [SerializeField] private Transform NE;
-        [SerializeField] private Transform SW;
-        [SerializeField] private Transform SE;
-        private float attackDelay;        
+        [SerializeField] private Transform nw;
+        [SerializeField] private Transform ne;
+        [SerializeField] private Transform sw;
+        [SerializeField] private Transform se;
+        
+        [Header("Projectile")]
+        [SerializeField] private float missileTargetChangeTime;
+        [SerializeField] private float newMissileTargetRadius;
+        [SerializeField] private Transform gun;
+        private Projectile _proj;
+        private float _lastTargetChange;
+        
+        private float _attackDelay;
         public override void Init() 
         {
             base.Init();
-            DeathDuration = 3.0f;
+            
             BuriedAmount = 3.0f;
-            attackDelay = 0.0f;
-            State = EnemyState.Moving;
+            _attackDelay = 0.0f;
+            
+            _lastTargetChange = 0f;
+            _proj = null;
+        }
+        
+        protected override void Attack(PlayerDamageable toDamage)
+        {
+            GameObject obj = ProjectilePool.GetInstance().GetFromPool(projectileType, muzzle?.position ?? transform.position,
+                Quaternion.LookRotation(TargetPosition - (muzzle?.position ?? transform.position)));
+            obj.transform.parent = EnemyManager.transform;
+
+            if (!obj.TryGetComponent(out _proj)) Debug.LogError("Projectile component not found");
+            
+            _proj.Init();
+            _proj.projectileType = projectileType;
+            _proj.TargetPosition = TargetPosition;
+            _proj.ToDamage = toDamage;
+            _proj.Damage = attackDamage;
         }
 
         protected override float Heuristic(Node start, Node end)
@@ -35,11 +63,11 @@ namespace Enemies
 
         protected override void UpdateState()
         {
-            attackDelay += Time.deltaTime;
-            if (State is EnemyState.Moving) attackDelay = 0.0f;
-            else if (attackDelay >= 10.0f)
+            _attackDelay += Time.deltaTime;
+            if (State is EnemyState.Moving) _attackDelay = 0.0f;
+            else if (_attackDelay >= 10.0f)
             {
-                attackDelay = 0.0f;
+                _attackDelay = 0.0f;
                 State = EnemyState.Moving;
             }
         }
@@ -52,6 +80,7 @@ namespace Enemies
             //primaryRB.AddForce(test * 20f, ForceMode.Impulse);
             
             DisallowShooting = !Grounded;
+            
             // Debug.Log($"transform.position.y = {transform.position.y} --- (MapManager.GetInstance().GetHeight(transform.position) + groundedOffset) = ${(MapManager.GetInstance().GetHeight(transform.position) + groundedOffset)}");
             // if (transform.position.y < (MapManager.GetInstance().GetHeight(transform.position) + groundedOffset))
             // {
@@ -77,44 +106,55 @@ namespace Enemies
 
             if (transform.position.y < (MapManager.GetInstance().GetHeight(transform.position) + (transform.lossyScale.y * 0.6f)))
             {
-                primaryRB.linearVelocity = Vector3.zero;
+                primaryRb.linearVelocity = Vector3.zero;
                 transform.position = new Vector3(transform.position.x, MapManager.GetInstance().GetHeight(transform.position) + (transform.lossyScale.y), transform.position.z);
             }
+            
+            _lastTargetChange += Time.deltaTime;
 
-            // gun rotation
+            if (_proj)
+            {
+                if (!_proj.gameObject.activeInHierarchy) _proj = null;
+                else if (_lastTargetChange > missileTargetChangeTime)
+                {
+                    var randomCircle = Random.insideUnitCircle.normalized * newMissileTargetRadius;
+                    _proj.TargetPosition = _proj.transform.position + new Vector3(randomCircle.x, 0f, randomCircle.y);
+                }
+            }
+            
             switch (State)
             {
                 case EnemyState.Moving:
                 {
-                    Mmodel.SetActive(false);
-                    Msphere.SetActive(true);
+                    mmodel.SetActive(false);
+                    msphere.SetActive(true);
                     if (Grounded)
                     {
-                        float nwHeight = MapManager.GetInstance().GetHeight(Msphere.transform.position + NW.localPosition);
-                        float neHeight = MapManager.GetInstance().GetHeight(Msphere.transform.position + NE.localPosition);
-                        float swHeight = MapManager.GetInstance().GetHeight(Msphere.transform.position + SW.localPosition);
-                        float seHeight = MapManager.GetInstance().GetHeight(Msphere.transform.position + SE.localPosition);
+                        float nwHeight = MapManager.GetInstance().GetHeight(msphere.transform.position + nw.localPosition);
+                        float neHeight = MapManager.GetInstance().GetHeight(msphere.transform.position + ne.localPosition);
+                        float swHeight = MapManager.GetInstance().GetHeight(msphere.transform.position + sw.localPosition);
+                        float seHeight = MapManager.GetInstance().GetHeight(msphere.transform.position + se.localPosition);
                         float sum = nwHeight +neHeight +swHeight +seHeight;
                         nwHeight = 1 - (nwHeight / sum);
                         neHeight = 1 - (neHeight / sum);
                         swHeight = 1 - (swHeight / sum);
                         seHeight = 1 - (seHeight / sum);
-                        Vector3 dir = (NW.localPosition * nwHeight) + (NE.localPosition * neHeight) + (SW.localPosition * swHeight) + (SE.localPosition * seHeight);
+                        Vector3 dir = (nw.localPosition * nwHeight) + (ne.localPosition * neHeight) + (sw.localPosition * swHeight) + (se.localPosition * seHeight);
                         if (dir.magnitude >= tolerance) dir = dir.normalized;
                         else dir = Vector3.zero;
-                        primaryRB.AddForce(dir * rollSpeed, ForceMode.VelocityChange);
+                        primaryRb.AddForce(dir * rollSpeed, ForceMode.VelocityChange);
                         //primaryRB.AddForce(Vector3.down * 9.81f, ForceMode.Acceleration);
                     }
                     break;
                 }
                 case EnemyState.AttackCore:
-                    Mmodel.SetActive(true);
-                    Msphere.SetActive(false);
+                    mmodel.SetActive(true);
+                    msphere.SetActive(false);
                     break;
                 case EnemyState.AttackHands:
                 {
-                    Mmodel.SetActive(true);
-                    Msphere.SetActive(false);
+                    mmodel.SetActive(true);
+                    msphere.SetActive(false);
                     break;
                 }
             }
